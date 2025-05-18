f7d42e1716d9b42fb0f7b469f2a5915087a002c9
Omkar Bhargude <omkarbhargude29@gmail.com>
2025-05-18 23:00:20 +0530

    Add files via upload

---
+++ program4.java
@@ -0,0 +1,23 @@
+
+class Display
+{
+    public void Display()
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 5; iCnt++)
+        {
+            System.out.println("JAY GANESH...");
+        }
+    }
+}
+class program4
+{
+    public static void main(String A[])
+    {
+        Display dobj = new Display();
+
+        dobj.Display();
+
+    }
+}
