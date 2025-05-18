d961e1e7af0176845c620ad0dfe8b19efd369e4e
Omkar Bhargude <omkarbhargude29@gmail.com>
2025-05-18 23:00:49 +0530

    Add files via upload

---
+++ program6.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Display
+{
+    public void Display(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            System.out.println("HELLO");
+        }
+    }
+}
+class program6
+{
+    public static void main(String A[])
+    {
+        int iValue = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the frequency :");
+        iValue = sobj.nextInt();
+
+        Display dobj = new Display();
+
+        dobj.Display(iValue);
+
+    }
+}
